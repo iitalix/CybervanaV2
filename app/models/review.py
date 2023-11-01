a4a841f782614852de52e4f4ec6a95a8781bbf4f
Lan <nolan.oribello@gmail.com>
2023-10-31 23:04:32 -0700

    added User, Vehicle, and Review models.

---
+++ app/models/review.py
@@ -0,0 +1,26 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable = False)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('vehicles.id')), nullable = False)
+    review = db.Column(db.String(1000), nullable = False)
+    created_at = db.Column(db.Date, nullable = False)
+    stars = db.Column(db.Integer, nullable=False)
+    my_review_user_id = db.relationship("User", back_populates = "my_review_id")
+    my_review_vehicle_id = db.relationship("Vehicle", back_populates = "my_vehicle_review_id")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "userId": self.user_id,
+            "vehicleId": self.vehicle_id,
+            "review": self.review,
+            "createdAt": self.created_at,
+            "users": self.my_review_user_id.to_dict()
+        }
--- migrations/versions/cc24feaeb71a_.py
+++ migrations/versions/cc24feaeb71a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 38f76c851f23
+Revision ID: cc24feaeb71a
 Revises: 
-Create Date: 2023-10-31 19:49:28.964262
+Create Date: 2023-10-31 21:43:29.223363
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '38f76c851f23'
+revision = 'cc24feaeb71a'
 down_revision = None
 branch_labels = None
 depends_on = None
