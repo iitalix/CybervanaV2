b948229bad4cf8903d189a06e48898f01749c735
Lan <nolan.oribello@gmail.com>
2023-11-02 23:58:25 -0700

    added additional stylling and conditional rendering. tested successfully.

--- react-app/src/components/Vehicles/VehicleCard/index.js
+++ react-app/src/components/Vehicles/VehicleCard/index.js
@@ -9,41 +9,36 @@ export default function VehicleCard({vehicle, user}) {
       <img src={`${vehicle.photoUrl}`} alt="vehicle" className="card-image" />
 
       <div id="card-details-buttons-container">
+
         <div className="card-details">
-          <div id="card-make-model">
+          <div className="card-make-model">
             {vehicle.make} {vehicle.model}
           </div>
-          <div>{`$${vehicle.price}`}</div>
-
-          {/* {vehicle.avgRating === "NaN" && (
-              <div id="star-new">
-                <i className="fa-solid fa-star"></i>
-                <div>New</div>
-              </div>
-            )}
-
-            {vehicle.avgRating !== "NaN" && (
-              <div>
-                <i className="fa-solid fa-star"></i>
-                {vehicle.avgRating}
-              </div>
-            )} */}
+          <div className="card-make-model">{`$${vehicle.price}`}</div>
         </div>
 
-        <div className="update-delete-container">
-          <div id="update-delete-buttons">
-            <OpenModalButton
-              buttonText="Update"
-              id="update"
-              modalComponent={<UpdateVehicleModal vehicleId={vehicle.id} />}
-            />
-            <OpenModalButton
-              buttonText="Delete"
-              id="delete"
-              modalComponent={<DeleteVehicleModal vehicleId={vehicle.id} />}
-            />
+        {user.id === vehicle.ownerId &&
+
+          <div className="update-delete-container">
+            <div id="update-delete-buttons">
+              <OpenModalButton
+                buttonText="Update"
+                id="update"
+                modalComponent={<UpdateVehicleModal vehicleId={vehicle.id} />}
+              />
+              <OpenModalButton
+                buttonText="Delete"
+                id="delete"
+                modalComponent={<DeleteVehicleModal vehicleId={vehicle.id} />}
+              />
+            </div>
           </div>
-        </div>
+
+
+        }
+
+
+
       </div>
     </div>
   );
