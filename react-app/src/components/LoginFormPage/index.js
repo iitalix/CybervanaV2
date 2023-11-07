40288779fbd4c17d1a4f6597661ae39728508545
Lan <nolan.oribello@gmail.com>
2023-11-07 10:13:40 -0800

    added minor changes to validation styling

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -25,11 +25,14 @@ function LoginFormPage() {
     <>
       <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
+        <div>
+          {errors.map((error) => (
+            <>
+              <div className="list-errors">{error}</div>
+            </>
           ))}
-        </ul>
+
+        </div>
         <label>
           Email
           <input
