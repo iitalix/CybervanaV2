93a03462eedb95189811a101cc7907527fb27747
Lan <nolan.oribello@gmail.com>
2023-12-28 14:34:52 -0800

    edited init file and fixed add item functionality

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.vehicles_routes import vehicles_routes
 from .api.reviews_routes import reviews_routes
+from .api.items_routes import items_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -32,6 +33,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(vehicles_routes, url_prefix='/api/vehicles')
 app.register_blueprint(reviews_routes, url_prefix='/api/reviews')
+app.register_blueprint(items_routes, url_prefix='/api/items')
 db.init_app(app)
 Migrate(app, db)
 
